e4138d1ff25438686142237802b31a4bc5f13edb
ferafael <circuncisaofabio@icloud.com>
2022-01-20 20:02:37 -0300

    comentarios

---
+++ aula-1/comentario.js
@@ -0,0 +1,16 @@
+
+
+function soma(numero,numero2,result){
+    var numero =10;
+    var numero2=10;
+    var result;
+
+    result = numero + numero2
+    console.log(result);
+}
+
+console.log(soma())
+/*console.log(soma());
+ console.log('Meu nome é' ,nome,'.Estou aprendendo JS as ',10,' da manhã'); comentario a linha toda*/
+
+// console.log('Meu nome é' ,nome,'.Estou aprendendo JS as ',10,' da manhã'); comentario de uma linha
