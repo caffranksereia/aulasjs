7fc08dcc76c451c740816045454f925828f0f806
ferafael <circuncisaofabio@icloud.com>
2022-01-24 18:05:12 -0300

    exercicios de let e const

---
+++ aula-1/Exercicios.js
@@ -0,0 +1,19 @@
+const name='Alvoro';
+const sobrenome='javascripton';
+const idade='28';
+const peso=84;
+const altura=1.80;
+let imc;
+let anoNascimento;
+
+
+imc =peso/ (altura*altura)
+// console.log(imc)
+
+anoNascimento = 2022 - 28
+
+// template string
+console.log(`olá meu nome é: ${name} ${sobrenome} peso ${peso},minha altura ${altura} e o meu imc é ${imc}
+o ano qeu nasci é ${anoNascimento}` )
+
+
