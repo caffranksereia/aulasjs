ce97d40358f9a4d5b602aeb7e6f437c11924b8ec
ferafael <circuncisaofabio@icloud.com>
2022-01-24 18:28:21 -0300

    let vs var

---
+++ aula-1/LetvsVar.js
@@ -0,0 +1,10 @@
+// let nome='caf'// uma variavel que pela ecmascript 2015(es6) não pode ser alterado
+// let nome ='sereia'
+
+
+
+var nome = 'caf' // var deixa alterar a variavel
+var nome ='sereia'
+
+
+console.log(nome)
