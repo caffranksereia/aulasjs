283ad22ec2bfe6272241daaeecb8832940495272
ferafael <circuncisaofabio@icloud.com>
2022-01-25 19:46:50 -0300

    tipos de dados primitivos

---
+++ aula-1/tiposdedadosprimitivos.js
@@ -0,0 +1,18 @@
+//posso ter uma variavel..
+const nome ='fabio'// string simples aspas
+const nome1 ="fabio" // string duble aspas
+const nome2 = `fabio` // string acrasiadao
+
+const number = 1 // varivel number
+const number1 =1.89 // variavale number
+
+let nomeAluno // receber o valor underfined = não aponta para local nenhum da memoria
+let sobrenomeAluno = null // NULO não aponta para nenhum local da memoria
+
+const boolean = true // tipo boolean = true or false (logico)
+
+
+//se quiser saber qual é o tipo da variavel 
+console.log(typeof nome) //coloca o nome da variavel 
+console.log(typeof nome, nome) //coloca o nome da variavel e o valor
+ 
