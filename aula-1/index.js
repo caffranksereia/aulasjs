662407485d12403913203bfec675fa0fcb588b1d
ferafael <circuncisaofabio@icloud.com>
2022-02-01 16:59:45 -0300

    calculo usando o prompt do browser

---
+++ aula-1/index.js
@@ -0,0 +1,11 @@
+let num1 = prompt('digita o numero:');
+let num2 = prompt('digita o proximo numero');
+
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+
+let soma;
+
+soma = num1+num2
+
+alert(soma)
