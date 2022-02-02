cb3f0eb91058f12b2da629c1d0941950714f14fa
ferafael <circuncisaofabio@icloud.com>
2022-02-02 17:06:57 -0300

    exercicios de variaveis

---
+++ aula-1/exercioVariaveis.js
@@ -0,0 +1,18 @@
+let varA ='A';
+let varB ='B';
+let varC = 'C';
+
+// const tempA = varA;
+
+// varA = varB;
+// varB = varC;
+// varC = tempA;
+
+
+//ou
+
+
+[varA,varB,varC]=[varB,varC,varA]
+
+
+console.log(varA,varB, varC);
