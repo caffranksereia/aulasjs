ac8dbd0491d5e9c784e9aa6fc516c9bd2ff58cf2
ferafael <circuncisaofabio@icloud.com>
2022-02-16 21:24:27 -0300

    calculadora

---
+++ calculadora/calculadora.js
@@ -0,0 +1,28 @@
+function somar(num1, num2){
+    let resultado;
+
+    resultado = num1 + num2;
+    return resultado
+}
+function dividir(num1, num2){
+    let resultado;
+
+    resultado = num1 / num2;
+    return resultado
+}
+function subtrair(num1, num2){
+    let resultado;
+
+    resultado = num1 - num2;
+    return resultado
+}
+function multiplicacao(num1, num2){
+    let resultado;
+
+    resultado = num1 - num2;
+    return resultado
+}
+console.log(somar(2,5))
+console.log(subtrair(2,5))
+console.log(multiplicacao(2,5))
+console.log(dividir(2,5))
