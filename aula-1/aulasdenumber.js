2e1f4ea1eb66e24978320ed81b35d434130c341c
ferafael <circuncisaofabio@icloud.com>
2022-02-16 21:23:36 -0300

    aulas de numeros

---
+++ aula-1/aulasdenumber.js
@@ -0,0 +1,11 @@
+let num1=1250;
+let num2= 25026;
+
+
+
+
+console.log(num1 + num2)
+console.log(num1.toString(2))
+console.log(num2.toString(2))
+console.log(num1.toFixed(2))
+console.log(Number.isInteger(num1))
